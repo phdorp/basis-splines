c10f6c98c3cb0f6028fc55236a7500672264afad
Philip <philip.dorpmueller@web.de>
2025-04-01 10:40:17 +0200

    docs: short description of tests

--- test/basisTest.cpp
+++ test/basisTest.cpp
@@ -11,6 +11,10 @@ protected:
   void SetUp() {}
 };
 
+/**
+ * @brief Test the evaluation of basis functions of order 1.
+ *
+ */
 TEST_F(BasisTest, BasisEvalOrder1) {
   const Eigen::ArrayXd knots{{0.0, 0.0, 0.0, 0.5, 0.5, 1.0, 1.0, 1.0}};
   const int order{1};
@@ -25,6 +29,10 @@ TEST_F(BasisTest, BasisEvalOrder1) {
   expectAllClose(valuesEst, valuesGtr, 1e-10);
 }
 
+/**
+ * @brief Test the evaluation of basis functions of order 2.
+ *
+ */
 TEST_F(BasisTest, BasisEvalOrder2) {
   const Eigen::ArrayXd knots{{0.0, 0.0, 0.0, 0.5, 0.5, 1.0, 1.0, 1.0}};
   const int order{2};
@@ -39,6 +47,10 @@ TEST_F(BasisTest, BasisEvalOrder2) {
   expectAllClose(valuesEst, valuesGtr, 1e-10);
 }
 
+/**
+ * @brief Test the determintation of greville sites for basis functions of order 2.
+ *
+ */
 TEST_F(BasisTest, GrevilleOrder2) {
   const Eigen::ArrayXd knots{{0.0, 0.0, 0.0, 0.5, 0.5, 1.0, 1.0, 1.0}};
   const int order{2};
@@ -50,6 +62,10 @@ TEST_F(BasisTest, GrevilleOrder2) {
   expectAllClose(valuesEst, valuesGtr, 1e-10);
 }
 
+/**
+ * @brief Test the determination of breakpoints for basis functions of order 2.
+ *
+ */
 TEST_F(BasisTest, BreakpointsOrder2) {
   const Eigen::ArrayXd knots{{0.0, 0.0, 0.5, 1.0, 1.0}};
   const int order{2};
@@ -64,6 +80,10 @@ TEST_F(BasisTest, BreakpointsOrder2) {
   expectAllClose(valuesEst.second, valuesGtr.second, 1e-10);
 }
 
+/**
+ * @brief Combine two spline bases of order 2.
+ *
+ */
 TEST_F(BasisTest, CombineOrder2) {
   const int order{2};
   const Eigen::ArrayXd knotsA{{0.0, 0.0, 0.2, 0.2, 0.5, 1.0, 1.0}};
