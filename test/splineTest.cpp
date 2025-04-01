24832797c991d822909a456d41d046fa94e99df0
Philip <philip.dorpmueller@web.de>
2025-04-01 16:34:59 +0200

    tests: add tests for spline sum and product

--- test/splineTest.cpp
+++ test/splineTest.cpp
@@ -36,10 +36,64 @@ TEST_F(SplineTest, SplineEvalOrder1) {
   expectAllClose(valuesEst, valuesGtr, 1e-6);
 }
 
+/**
+ * @brief Test summing two splines of order 3.
+ *
+ */
+TEST_F(SplineTest, SplineSumOrder3)
+{
+  const int order{3};
+  const Eigen::ArrayXd knotsL{{0.0, 0.0, 0.0, 0.5, 1.0, 1.0}};
+  std::shared_ptr<Basis> basisL{std::make_shared<Basis>(knotsL, order)};
+  const Eigen::ArrayXd coeffsL {Eigen::ArrayXd::Random(knotsL.size() - order)};
+  const Spline splineL {basisL, coeffsL};
+
+
+  const Eigen::ArrayXd knotsR{{0.0, 0.0, 0.0, 0.25, 0.5, 0.8, 1.0, 1.0}};
+  std::shared_ptr<Basis> basisR{std::make_shared<Basis>(knotsR, order)};
+  const Eigen::ArrayXd coeffsR {Eigen::ArrayXd::Random(knotsR.size() - order)};
+  const Spline splineR {basisR, coeffsR};
+
+  const Eigen::ArrayXd points {Eigen::ArrayXd::LinSpaced(101, 0.0, 1.0)};
+  const Eigen::ArrayXd valuesGtr {splineL(points) + splineR(points)};
+
+  const Spline spline {splineL.add(splineR)};
+  const Eigen::ArrayXd valuesEst {spline(points)};
+
+  expectAllClose(valuesGtr, valuesEst, 1e-10);
+}
+/**
+ * @brief Test multiplying two splines of order 3.
+ *
+ */
+TEST_F(SplineTest, SplineProdOrder3)
+{
+  const int order{3};
+  const Eigen::ArrayXd knotsL{{0.0, 0.0, 0.0, 0.5, 1.0, 1.0}};
+  std::shared_ptr<Basis> basisL{std::make_shared<Basis>(knotsL, order)};
+  const Eigen::ArrayXd coeffsL {Eigen::ArrayXd::Random(knotsL.size() - order)};
+  const Spline splineL {basisL, coeffsL};
+
+
+  const Eigen::ArrayXd knotsR{{0.0, 0.0, 0.0, 0.25, 0.5, 0.8, 1.0, 1.0}};
+  std::shared_ptr<Basis> basisR{std::make_shared<Basis>(knotsR, order)};
+  const Eigen::ArrayXd coeffsR {Eigen::ArrayXd::Random(knotsR.size() - order)};
+  const Spline splineR {basisR, coeffsR};
+
+  const Eigen::ArrayXd points {Eigen::ArrayXd::LinSpaced(101, 0.0, 1.0)};
+  const Eigen::ArrayXd valuesGtr {splineL(points) * splineR(points)};
+
+  const Spline spline {splineL.prod(splineR)};
+  const Eigen::ArrayXd valuesEst {spline(points)};
+
+  expectAllClose(valuesGtr, valuesEst, 1e-10);
+}
+
 }; // namespace Internal
 }; // namespace BasisSplines
 
 int main(int argc, char **argv) {
+  std::srand(0);
   testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
 }
