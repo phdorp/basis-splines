4678486bba56feeea9134e801d5a34bc1703ac41
dorpmueller <philip.dorpmueller@tu-dortmund.de>
2025-05-10 13:13:47 +0200

    refactor: add bases of order 2 and 3, and points as members
    Share same bases and evaluation points among tests.
    Adapt docstrings accordingly.

--- test/splineTest.cpp
+++ test/splineTest.cpp
@@ -9,22 +9,27 @@ namespace BasisSplines {
 namespace Internal {
 class SplineTest : public TestBase {
 protected:
-  void SetUp() {}
+  void SetUp() {
+
+  }
+
+  const Eigen::ArrayXd m_knotsO2{{0.0, 0.0, 0.5, 1.0, 1.0}};
+  std::shared_ptr<Basis> m_basisO2{std::make_shared<Basis>(m_knotsO2, 2)};
+
+  const Eigen::ArrayXd m_knotsO3{{0.0, 0.0, 0.0, 0.5, 1.0, 1.0, 1.0}};
+  std::shared_ptr<Basis> m_basisO3{std::make_shared<Basis>(m_knotsO3, 3)};
+
+  const Eigen::ArrayXd m_points{Eigen::ArrayXd::LinSpaced(101, 0.0, 1.0)};
 };
 
 /**
  * @brief Test piecewise linear spline function.
  *
  */
-TEST_F(SplineTest, SplineEvalOrder1) {
-  // setup basis of order 2 with continuity 0, 1, 0
-  const Eigen::ArrayXd knots{{0.0, 0.0, 0.5, 1.0, 1.0}};
-  const int order{2};
-  std::shared_ptr<Basis> basis{std::make_shared<Basis>(knots, order)};
-
+TEST_F(SplineTest, SplineEvalO1) {
   // setup spline function of linear segments
   const Eigen::ArrayXd coeffs{{0.0, 1.0, 0.25}};
-  const Spline spline{basis, coeffs};
+  const Spline spline{m_basisO2, coeffs};
 
   // evaluate spline functions
   const Eigen::ArrayXd points{{0.0, 0.25, 0.5, 1.0}};
@@ -40,23 +45,23 @@ TEST_F(SplineTest, SplineEvalOrder1) {
  * @brief Test summing two splines of order 3.
  *
  */
-TEST_F(SplineTest, SplineSumOrder3) {
-  const int order{3};
-  const Eigen::ArrayXd knotsL{{0.0, 0.0, 0.0, 0.5, 1.0, 1.0}};
-  std::shared_ptr<Basis> basisL{std::make_shared<Basis>(knotsL, order)};
-  const Eigen::ArrayXd coeffsL{Eigen::ArrayXd::Random(knotsL.size() - order)};
-  const Spline splineL{basisL, coeffsL};
+TEST_F(SplineTest, SplineSumO3) {
+  // instatiate left operand spline of order 3
+  const Eigen::ArrayXd coeffsL{Eigen::ArrayXd::Random(m_basisO3->dim())};
+  const Spline splineL{m_basisO3, coeffsL};
 
+  // instantiate right operand spline of order 3
   const Eigen::ArrayXd knotsR{{0.0, 0.0, 0.0, 0.25, 0.5, 0.8, 1.0, 1.0}};
-  std::shared_ptr<Basis> basisR{std::make_shared<Basis>(knotsR, order)};
-  const Eigen::ArrayXd coeffsR{Eigen::ArrayXd::Random(knotsR.size() - order)};
+  std::shared_ptr<Basis> basisR{std::make_shared<Basis>(knotsR, 3)};
+  const Eigen::ArrayXd coeffsR{Eigen::ArrayXd::Random(basisR->dim())};
   const Spline splineR{basisR, coeffsR};
 
-  const Eigen::ArrayXd points{Eigen::ArrayXd::LinSpaced(101, 0.0, 1.0)};
-  const Eigen::ArrayXd valuesGtr{splineL(points) + splineR(points)};
+  // get gt from spline sum
+  const Eigen::ArrayXd valuesGtr{splineL(m_points) + splineR(m_points)};
 
+  // get estimate from sum spline
   const Spline spline{splineL.add(splineR)};
-  const Eigen::ArrayXd valuesEst{spline(points)};
+  const Eigen::ArrayXd valuesEst{spline(m_points)};
 
   expectAllClose(valuesGtr, valuesEst, 1e-10);
 }
@@ -65,23 +70,23 @@ TEST_F(SplineTest, SplineSumOrder3) {
  * @brief Test multiplying two splines of order 3.
  *
  */
-TEST_F(SplineTest, SplineProdOrder3) {
-  const int order{3};
-  const Eigen::ArrayXd knotsL{{0.0, 0.0, 0.0, 0.5, 1.0, 1.0}};
-  std::shared_ptr<Basis> basisL{std::make_shared<Basis>(knotsL, order)};
-  const Eigen::ArrayXd coeffsL{Eigen::ArrayXd::Random(knotsL.size() - order)};
-  const Spline splineL{basisL, coeffsL};
+TEST_F(SplineTest, SplineProdO3) {
+  // instatiate left operand spline of order 3
+  const Eigen::ArrayXd coeffsL{Eigen::ArrayXd::Random(m_basisO3->dim())};
+  const Spline splineL{m_basisO3, coeffsL};
 
+  // instantiate right operand spline of order 3
   const Eigen::ArrayXd knotsR{{0.0, 0.0, 0.0, 0.25, 0.5, 0.8, 1.0, 1.0}};
-  std::shared_ptr<Basis> basisR{std::make_shared<Basis>(knotsR, order)};
-  const Eigen::ArrayXd coeffsR{Eigen::ArrayXd::Random(knotsR.size() - order)};
+  std::shared_ptr<Basis> basisR{std::make_shared<Basis>(knotsR, 3)};
+  const Eigen::ArrayXd coeffsR{Eigen::ArrayXd::Random(basisR->dim())};
   const Spline splineR{basisR, coeffsR};
 
-  const Eigen::ArrayXd points{Eigen::ArrayXd::LinSpaced(101, 0.0, 1.0)};
-  const Eigen::ArrayXd valuesGtr{splineL(points) * splineR(points)};
+  // get gt from spline product
+  const Eigen::ArrayXd valuesGtr{splineL(m_points) * splineR(m_points)};
 
+  // get estimate from product spline
   const Spline spline{splineL.prod(splineR)};
-  const Eigen::ArrayXd valuesEst{spline(points)};
+  const Eigen::ArrayXd valuesEst{spline(m_points)};
 
   expectAllClose(valuesGtr, valuesEst, 1e-10);
 }
@@ -90,24 +95,23 @@ TEST_F(SplineTest, SplineProdOrder3) {
  * @brief Test multiplying splines of order 4 and 3.
  *
  */
-TEST_F(SplineTest, SplineProdOrderL3R4) {
-  const int orderL{3};
-  const Eigen::ArrayXd knotsL{{0.0, 0.0, 0.0, 0.5, 1.0, 1.0}};
-  std::shared_ptr<Basis> basisL{std::make_shared<Basis>(knotsL, orderL)};
-  const Eigen::ArrayXd coeffsL{Eigen::ArrayXd::Random(knotsL.size() - orderL)};
-  const Spline splineL{basisL, coeffsL};
+TEST_F(SplineTest, SplineProdO3O4) {
+  // instatiate left operand spline of order 3
+  const Eigen::ArrayXd coeffsL{Eigen::ArrayXd::Random(m_basisO3->dim())};
+  const Spline splineL{m_basisO3, coeffsL};
 
-  const int orderR{4};
+  // instantiate right operand spline of order 4
   const Eigen::ArrayXd knotsR{{0.0, 0.0, 0.0, 0.25, 0.5, 0.8, 1.0, 1.0}};
-  std::shared_ptr<Basis> basisR{std::make_shared<Basis>(knotsR, orderR)};
-  const Eigen::ArrayXd coeffsR{Eigen::ArrayXd::Random(knotsR.size() - orderR)};
+  std::shared_ptr<Basis> basisR{std::make_shared<Basis>(knotsR, 4)};
+  const Eigen::ArrayXd coeffsR{Eigen::ArrayXd::Random(basisR->dim())};
   const Spline splineR{basisR, coeffsR};
 
-  const Eigen::ArrayXd points{Eigen::ArrayXd::LinSpaced(101, 0.0, 1.0)};
-  const Eigen::ArrayXd valuesGtr{splineL(points) * splineR(points)};
+  // get gt from spline product
+  const Eigen::ArrayXd valuesGtr{splineL(m_points) * splineR(m_points)};
 
+  // get estimate from product spline
   const Spline spline{splineL.prod(splineR)};
-  const Eigen::ArrayXd valuesEst{spline(points)};
+  const Eigen::ArrayXd valuesEst{spline(m_points)};
 
   expectAllClose(valuesGtr, valuesEst, 1e-10);
 }
@@ -116,18 +120,18 @@ TEST_F(SplineTest, SplineProdOrderL3R4) {
  * @brief Test derivative spline of order 3.
  *
  */
-TEST_F(SplineTest, SplineDerivOrder3) {
-  const int order{3};
-  const Eigen::ArrayXd knots{{0.0, 0.0, 0.0, 0.5, 1.0, 1.0, 1.0}};
-  std::shared_ptr<Basis> basis{std::make_shared<Basis>(knots, order)};
-  const Eigen::ArrayXd coeffs{Eigen::ArrayXd::Random(knots.size() - order)};
-  const Spline spline{basis, coeffs};
+TEST_F(SplineTest, SplineDerivO3) {
+  // instantiate spline of order 3
+  const Eigen::ArrayXd coeffs{Eigen::ArrayXd::Random(m_basisO3->dim())};
+  const Spline spline{m_basisO3, coeffs};
 
+  // get gt first derivative with finite differences
   const double step{1e-8};
   const Eigen::ArrayXd points{Eigen::ArrayXd::LinSpaced(101, 0.0, 1.0 - step)};
   const Eigen::ArrayXd valuesGtr{(spline(points + step) - spline(points)) /
                                  step};
 
+  // get estimate from spline derivative
   const Eigen::ArrayXd valuesEst{spline.derivative()(points)};
 
   expectAllClose(valuesGtr, valuesEst, 1e-6);
@@ -137,16 +141,17 @@ TEST_F(SplineTest, SplineDerivOrder3) {
  * @brief Test integral spline of order 2.
  *
  */
-TEST_F(SplineTest, SplineIntOrder2) {
+TEST_F(SplineTest, SplineIntO2) {
+  // instantiate a spline of order 2
   const int order{2};
   const Eigen::ArrayXd knots{{0.0, 0.0, 1.0, 1.0}};
   std::shared_ptr<Basis> basis{std::make_shared<Basis>(knots, order)};
   const Eigen::ArrayXd coeffs{{0, 1}};
   const Spline spline{basis, coeffs};
 
-  const Eigen::ArrayXd points{Eigen::ArrayXd::LinSpaced(101, 0.0, 1.0)};
-  Eigen::ArrayXd valuesGtr {spline(points).pow(2) / 2};
-  const Eigen::ArrayXd valuesEst{spline.integral()(points)};
+  // spline integral ground truth
+  Eigen::ArrayXd valuesGtr {spline(m_points).pow(2) / 2};
+  const Eigen::ArrayXd valuesEst{spline.integral()(m_points)};
 
   expectAllClose(valuesGtr, valuesEst, 1e-6);
 }
@@ -155,37 +160,38 @@ TEST_F(SplineTest, SplineIntOrder2) {
  * @brief Test integral spline of order 3.
  *
  */
-TEST_F(SplineTest, SplineIntOrder3) {
-  const int order{3};
-  const Eigen::ArrayXd knots{{0.0, 0.0, 0.0, 0.5, 1.0, 1.0, 1.0}};
-  std::shared_ptr<Basis> basis{std::make_shared<Basis>(knots, order)};
-  const Eigen::ArrayXd coeffs{Eigen::ArrayXd::Random(knots.size() - order)};
-  const Spline spline{basis, coeffs};
+TEST_F(SplineTest, SplineIntO3) {
+  // instantiate spline of order 3
+  const Eigen::ArrayXd coeffs{Eigen::ArrayXd::Random(m_basisO3->dim())};
+  const Spline spline{m_basisO3, coeffs};
 
-  const Eigen::ArrayXd points{Eigen::ArrayXd::LinSpaced(101, 0.0, 1.0)};
-  const double step{points(points.size() - 1) / (points.size() - 1)};
-  Eigen::ArrayXd valuesGtr(points.size());
-  const Eigen::ArrayXd splineValues{spline(points)};
+  // get ground truth with numeric approximation
+  const double step{m_points(m_points.size() - 1) / (m_points.size() - 1)};
+  Eigen::ArrayXd valuesGtr(m_points.size());
+  const Eigen::ArrayXd splineValues{spline(m_points)};
   std::partial_sum(splineValues.begin(), splineValues.end(), valuesGtr.begin());
   valuesGtr *= step;
-  const Eigen::ArrayXd valuesEst{spline.integral()(points)};
+
+  // get estimate from spline integral
+  const Eigen::ArrayXd valuesEst{spline.integral()(m_points)};
 
   expectAllClose(valuesGtr, valuesEst, 1e-2);
 }
 
 TEST_F(SplineTest, InsertKnots) {
-  const int order{3};
-  const Eigen::ArrayXd knots{{0.0, 0.0, 0.0, 0.5, 1.0, 1.0, 1.0}};
-  std::shared_ptr<Basis> basis{std::make_shared<Basis>(knots, order)};
-  const Eigen::ArrayXd coeffs{Eigen::ArrayXd::Random(knots.size() - order)};
-  const Spline spline{basis, coeffs};
+  // instantiate spline of order 3
+  const Eigen::ArrayXd coeffs{Eigen::ArrayXd::Random(m_basisO3->dim())};
+  const Spline spline{m_basisO3, coeffs};
 
+  // insert knots
   const Eigen::ArrayXd knotsInsert{{0.4, 0.5, 0.6}};
   const Spline splineInsert{spline.insertKnots(knotsInsert)};
 
-  const Eigen::ArrayXd points{Eigen::ArrayXd::LinSpaced(101, 0.0, 1.0)};
-  const Eigen::ArrayXd valuesGtr{spline(points)};
-  const Eigen::ArrayXd valuesEst{splineInsert(points)};
+  // get ground truth from initial spline
+  const Eigen::ArrayXd valuesGtr{spline(m_points)};
+
+  // get estimate from result spline
+  const Eigen::ArrayXd valuesEst{splineInsert(m_points)};
 
   expectAllClose(valuesGtr, valuesEst, 1e-6);
 }
