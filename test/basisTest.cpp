79ecb4503b6f793794346831c2f1e1b2817781bb
dorpmueller <philip.dorpmueller@tu-dortmund.de>
2025-05-10 13:11:58 +0200

    tests: remove basis evaluation tests
    Obsolet since spline evaluation requires basis evaluation.

--- test/basisTest.cpp
+++ test/basisTest.cpp
@@ -30,40 +30,7 @@ TEST_F(BasisTest, BasisEvalOrder1) {
 }
 
 /**
- * @brief Test the evaluation of basis functions of order 2.
- *
- */
-TEST_F(BasisTest, BasisEvalOrder2) {
-  const Eigen::ArrayXd knots{{0.0, 0.0, 0.0, 0.5, 0.5, 1.0, 1.0, 1.0}};
-  const int order{2};
-  const Eigen::ArrayXd points{{0.1, 0.75}};
-
-  const Basis basis{knots, order};
-  const Eigen::ArrayXXd valuesEst{basis(points)};
-
-  const Eigen::ArrayXXd valuesGtr{{0.0, 0.8, 0.2, 0.0, 0.0, 0.0},
-                                  {0.0, 0.0, 0.0, 0.5, 0.5, 0.0}};
-
-  expectAllClose(valuesEst, valuesGtr, 1e-10);
-}
-
-/**
- * @brief Test the determintation of greville sites for basis functions of order 2.
- *
- */
-TEST_F(BasisTest, GrevilleOrder2) {
-  const Eigen::ArrayXd knots{{0.0, 0.0, 0.0, 0.5, 0.5, 1.0, 1.0, 1.0}};
-  const int order{2};
-
-  const Basis basis{knots, order};
-  const Eigen::ArrayXd valuesEst{basis.greville()};
-  const Eigen::ArrayXd valuesGtr{{0.0, 0.0, 0.5, 0.5, 1.0, 1.0}};
-
-  expectAllClose(valuesEst, valuesGtr, 1e-10);
-}
-
-/**
- * @brief Test the determination of breakpoints for basis functions of order 2.
+ * @brief Test the determination of breakpoints for basis functions of order 3.
  *
  */
 TEST_F(BasisTest, BreakpointsOrder2) {
