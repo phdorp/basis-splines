9030179db854d3e59f054aee934f8caa9c93efa3
Philip <philip.dorpmueller@web.de>
2025-05-16 18:42:47 +0200

    refactor: move test conditions from transform to basis

--- test/basisTest.cpp
+++ test/basisTest.cpp
@@ -2,16 +2,83 @@
 #include <gtest/gtest.h>
 
 #include "basisSplines/basis.h"
+#include "basisSplines/interpolate.h"
+#include "basisSplines/spline.h"
 #include "testBase.h"
 
 namespace BasisSplines {
 namespace Internal {
 class BasisTest : public TestBase {
 protected:
-  void SetUp() {}
+  static Eigen::ArrayXd polyO3(const Eigen::ArrayXd &points) {
+    return Eigen::ArrayXd{points.pow(2)};
+  }
 
-  const Eigen::ArrayXd m_knotsO3{{0.0, 0.0, 0.0, 0.5, 0.5, 1.0, 1.0, 1.0}};
-  Basis m_basisO3{m_knotsO3, 3};
+  static Eigen::ArrayXd polyO3Der(const Eigen::ArrayXd &points) {
+    return Eigen::ArrayXd{2 * points};
+  }
+
+  static Eigen::ArrayXd polyO3Dder(const Eigen::ArrayXd &points) {
+    return Eigen::ArrayXd::Zero(points.size()) + 2;
+  }
+
+  static Eigen::ArrayXd polyO3Int(const Eigen::ArrayXd &points) {
+    return Eigen::ArrayXd{points.pow(3) / 3};
+  }
+
+  static Eigen::ArrayXd polyO3Iint(const Eigen::ArrayXd &points) {
+    return Eigen::ArrayXd{points.pow(4) / 12};
+  }
+
+  // create basis of order 3
+  const Eigen::ArrayXd m_knotsO3{
+      {0.0, 0.0, 0.0, 0.5, 1.0, 1.0, 1.0}}; /**<< knots of order 3 basis */
+  std::shared_ptr<Basis> m_basisO3{
+      std::make_shared<Basis>(m_knotsO3, 3)}; /**<< order 3 basis */
+
+  // create order 3 interatpolation
+  const Interpolate m_interpolateO3{
+      m_basisO3}; /**<< interpolation for order 3 basis */
+
+  // create order 3 spline interpolating order 3 polynomial
+  const Spline m_splineO3{
+      m_basisO3, m_interpolateO3.fit(&polyO3)}; /**<< spline of order 3 */
+
+  // create order 3 derivative spline
+  std::shared_ptr<Basis> m_basisO3Der{std::make_shared<Basis>(
+      m_basisO3->orderDecrease())}; /**<< order 3 derivative basis */
+  const Interpolate m_interpolateO3Der{
+      m_basisO3Der}; /**<< order 3 derivative interpolation */
+  Spline m_splineO3Der{
+      m_basisO3Der,
+      m_interpolateO3Der.fit(&polyO3Der)}; /** order 3 derivative spline */
+
+  // create order 3 second derivative spline
+  std::shared_ptr<Basis> m_basisO3Dder{std::make_shared<Basis>(
+      m_basisO3Der->orderDecrease())}; /**<< order 3 second derivative basis */
+  const Interpolate m_interpolateO3Dder{
+      m_basisO3Dder}; /**<< order 3 second derivative interpolation */
+  Spline m_splineO3Dder{
+      m_basisO3Dder, m_interpolateO3Dder.fit(
+                         &polyO3Dder)}; /** order 3 second derivative spline */
+
+  // create order 3 integral spline
+  std::shared_ptr<Basis> m_basisO3Int{std::make_shared<Basis>(
+      m_basisO3->orderIncrease())}; /**<< order 3 integral basis */
+  const Interpolate m_interpolateO3Int{
+      m_basisO3Int}; /**<< order 3 integral interpolation */
+  Spline m_splineO3Int{
+      m_basisO3Int,
+      m_interpolateO3Int.fit(&polyO3Int)}; /** order 3 integral spline */
+
+  // create order 3 second integral spline
+  std::shared_ptr<Basis> m_basisO3Iint{std::make_shared<Basis>(
+      m_basisO3Int->orderIncrease())}; /**<< order 3 integral basis */
+  const Interpolate m_interpolateO3Iint{
+      m_basisO3Iint}; /**<< order 3 integral interpolation */
+  Spline m_splineO3Iint{
+      m_basisO3Iint,
+      m_interpolateO3Iint.fit(&polyO3Iint)}; /** order 3 integral spline */
 };
 
 /**
@@ -20,8 +87,8 @@ protected:
  *
  */
 TEST_F(BasisTest, GrevilleO3) {
-  const Eigen::ArrayXd valuesEst{m_basisO3.greville()};
-  const Eigen::ArrayXd valuesGtr{{0.0, 0.25, 0.5, 0.75, 1.0}};
+  const Eigen::ArrayXd valuesEst{m_basisO3->greville()};
+  const Eigen::ArrayXd valuesGtr{{0.0, 0.25, 0.75, 1.0}};
 
   expectAllClose(valuesEst, valuesGtr, 1e-10);
 }
@@ -32,9 +99,9 @@ TEST_F(BasisTest, GrevilleO3) {
  */
 TEST_F(BasisTest, BreakpointsO3) {
   const std::pair<Eigen::ArrayXd, Eigen::ArrayXi> valuesEst{
-      m_basisO3.breakpoints()};
+      m_basisO3->breakpoints()};
   const std::pair<Eigen::ArrayXd, Eigen::ArrayXi> valuesGtr{{{0.0, 0.5, 1.0}},
-                                                            {{0, 1, 0}}};
+                                                            {{0, 2, 0}}};
 
   expectAllClose(valuesEst.first, valuesGtr.first, 1e-10);
   expectAllClose(valuesEst.second, valuesGtr.second, 1e-10);
@@ -48,11 +115,11 @@ TEST_F(BasisTest, CombineO3O2) {
   const Eigen::ArrayXd knotsO2{{0.0, 0.0, 0.2, 0.5, 0.6, 1.0, 1.0}};
   const Basis basisO2{knotsO2, 2};
 
-  const Basis estimate{m_basisO3.combine(basisO2, m_basisO3.order())};
+  const Basis estimate{m_basisO3->combine(basisO2, m_basisO3->order())};
 
   const Basis groundTruth{
       {{0.0, 0.0, 0.0, 0.2, 0.2, 0.5, 0.5, 0.6, 0.6, 1.0, 1.0, 1.0}},
-      m_basisO3.order()};
+      m_basisO3->order()};
 
   expectAllClose(estimate.knots(), groundTruth.knots(), 1e-6);
   EXPECT_EQ(estimate.order(), groundTruth.order());
@@ -63,9 +130,9 @@ TEST_F(BasisTest, CombineO3O2) {
  *
  */
 TEST_F(BasisTest, ToKnotsO3) {
-  const auto [bps, conts] = m_basisO3.breakpoints();
+  const auto [bps, conts] = m_basisO3->breakpoints();
 
-  const Eigen::ArrayXd valuesEst{Basis::toKnots(bps, conts, m_basisO3.order())};
+  const Eigen::ArrayXd valuesEst{Basis::toKnots(bps, conts, m_basisO3->order())};
   const Eigen::ArrayXd valuesGtr{m_knotsO3};
 
   expectAllClose(valuesEst, valuesGtr, 1e-10);
