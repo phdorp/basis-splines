d831659af77fe21400eacd00df0f4c7d531ed22e
Philip <philip.dorpmueller@web.de>
2025-05-18 13:37:28 +0200

    tests: add test for kronecker product

--- test/mathTest.cpp
+++ test/mathTest.cpp
@@ -8,6 +8,7 @@ namespace BasisSplines {
 namespace Internal {
 class MathTest : public TestBase {
 protected:
+  const Eigen::ArrayXXd arrI22{{1, 0}, {0, 1}};
   const Eigen::ArrayXXd arr32{{1, 2}, {3, 4}, {5, 6}}; /**<< (3 x 2) matrix */
   const Eigen::ArrayXXd arr33{
       {1, 2, 3}, {4, 5, 6}, {7, 8, 9}}; /**<< (3 x 3) matrix */
@@ -25,5 +26,22 @@ TEST_F(MathTest, khatriRao3x6) {
 
   expectAllClose(valuesEst, valuesGtr, 1e-10);
 }
+
+/**
+ * @brief Test Kronecker product of (2 x 2) with (3 x 2) matrix.
+ *
+ */
+TEST_F(MathTest, kron6x4) {
+  Eigen::ArrayXXd valuesGtr{Eigen::ArrayXXd::Zero(
+      arrI22.rows() * arr32.rows(), arrI22.cols() * arr32.cols())};
+
+  valuesGtr.topLeftCorner(arr32.rows(), arr32.cols()) = arr32;
+  valuesGtr.bottomRightCorner(arr32.rows(), arr32.cols()) = arr32;
+
+  const Eigen::ArrayXXd valuesEst{kron(arrI22, arr32)};
+
+  expectAllClose(valuesEst, valuesGtr, 1e-10);
+}
+
 }; // namespace Internal
 }; // namespace BasisSplines
