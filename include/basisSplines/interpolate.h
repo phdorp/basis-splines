cde35a68540fc8204118c1007cd6f718df45f998
dorpmueller <philip.dorpmueller@tu-dortmund.de>
2025-05-30 19:10:52 +0200

    feat: fit spline coefficients to derivatives

--- include/basisSplines/interpolate.h
+++ include/basisSplines/interpolate.h
@@ -42,6 +42,60 @@ public:
   }
 
   /**
+   * @brief Determine coefficients that fit a spline function at the given
+   * "points" and the given "observations". The observations consist of an n
+   x
+   * m-array with n observations and derivatives until order m - 1.
+   *
+   * @param observations values and derivatives to fit the spline function.
+   * @param points evaluation points corresponding to the "observations".
+   * @return Eigen::ArrayXd spline coefficients fitting the observations.
+   */
+  template <
+      typename DecompositionType = Eigen::ColPivHouseholderQR<Eigen::MatrixXd>>
+  Eigen::ArrayXd fit(const std::vector<Eigen::VectorXd> &observations,
+                     const std::vector<Eigen::VectorXi> &derivOrders,
+                     const Eigen::ArrayXd &points) const {
+
+    // store transformation matrices and spline bases
+    std::vector<Eigen::MatrixXd> transforms(m_basis->order() - 1);
+    std::vector<Basis> bases(m_basis->order() - 1);
+
+    // TODO: iterative application of derivative based on previous result
+    int cOrder{};
+    for (Eigen::MatrixXd &transform : transforms) {
+      transform = m_basis->derivative(bases[cOrder], cOrder);
+      ++cOrder;
+    }
+
+    // evaluate basis functions at "points" and transform according to given
+    // derivartive
+    Eigen::MatrixXd basisValues(m_basis->dim(), m_basis->dim());
+    int cObs{};
+    int cRow{};
+    for (const Eigen::VectorXd &observation : observations) {
+      int cValue{};
+      for (double value : observation) {
+        basisValues(cRow++, Eigen::all) =
+            bases[derivOrders[cObs](cValue)](points(cObs)) *
+            transforms[derivOrders[cObs](cValue)];
+        ++cValue;
+      }
+      ++cObs;
+    }
+
+    // arrange observation in array
+    Eigen::VectorXd splineValues(m_basis->dim());
+    int cElem{};
+    for (const auto &observation : observations)
+      for (double value : observation)
+        splineValues(cElem++) = value;
+
+    // solve for spline coefficients
+    return DecompositionType{basisValues}.solve(splineValues);
+  }
+
+  /**
    * @brief Determine coefficients that fit a spline function to the given
    * process.
    *
